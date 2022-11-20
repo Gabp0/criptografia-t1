dbe876586996a09b08995e8ad5918d4e579fb9ba
gab <gabriel.pontarolo045@gmail.com>
2022-11-20 14:19:41 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string input;
+    cin >> input;
+    // cool encrypt logic
+    cout << input;
+
+    return 0;
+}
